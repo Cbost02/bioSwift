dc12de83814098ddb7ccaf70698d3c160c233669
Cromwell Bost <132315469+Cbost02@users.noreply.github.com>
2026-02-21 02:39:24 -0500

    Replace EmptyScreen with HomeScreen in main.dart

--- android/bioinformatics/lib/main.dart
+++ android/bioinformatics/lib/main.dart
@@ -11,13 +11,13 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: EmptyScreen(),
+      home: HomeScreen(),
     );
   }
 }
 
-class EmptyScreen extends StatelessWidget {
-  const EmptyScreen({super.key});
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
